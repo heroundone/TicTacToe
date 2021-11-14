b65da9afcf588c16e763787cf8a5559cde0a36df
Jason Bedini <jasonbedini@gmail.com>
2021-11-13 18:24:42 -0800

    game can be played and winner can be determined, no ties yet

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ const gameBoard = (() => {
         // create array to contain elements of gameBoard
         for (let i = 0; i < sizeOfBoard; i++) {
             let boardSpotsClone = boardSpots.cloneNode();
-            boardSpotsClone.textContent = 'X'
             theGameBoard.push(boardSpotsClone);
         };
 
@@ -24,16 +23,9 @@ const gameBoard = (() => {
         // create and retrieve array and create container to append to
         theGameBoard = createGameBoard();
         const container = document.getElementById('gameboard');
-        /*let marker = document.createElement('div');
-        marker.classList.add('marker');
-        marker.textContent = 'XO';*/
         
         // append
-        for(let i = 0; i < theGameBoard.length; i++) {
-            //markerClone = marker.cloneNode();
-            //theGameBoard[i].appendChild(markerClone);
-            container.appendChild(theGameBoard[i]);
-        }
+        theGameBoard.forEach((index) => container.appendChild(index));
     }
 
     return {
@@ -41,4 +33,142 @@ const gameBoard = (() => {
         };
 })();
 
+const displayController = (() => {
+    // keeps track of which marker('x' or 'o') to use
+    let tracker = 2;
+
+    // check subarrays in 'checkForWinner' function against these two arrays
+    let compareArray1 = [];
+    let compareArray2 = [];
+    for(let i = 0; i < 3; i++) {
+        compareArray1.push('X');
+        compareArray2.push('O');
+    }
+    let xCompareArray = compareArray1.join('');
+    let yCompareArray = compareArray2.join('');
+
+    // add event listener to each spot on the gameboard
+    const addClickToMark = () => {
+        let boardspots = document.querySelectorAll('.boardSpots');
+        let theGameBoard = Array.from(boardspots);
+        console.log(theGameBoard);
+        theGameBoard.forEach((index) => index.addEventListener('click', () => {
+            // check if the spot has been marked, if not then go ahead and mark it
+            if(index.textContent === '') {
+                if((tracker % 2) === 0) {
+                    index.textContent = 'X';
+                    tracker ++;
+                }
+                else {
+                    index.textContent = 'O';
+                    tracker ++;
+                };
+            };
+            if(tracker >= 7) {
+                let check = checkForWinner(theGameBoard);
+                if(check != false) {
+                    let winner = document.getElementById('winner');
+                    winner.textContent = check;
+                }
+            }
+        }))
+    };
+
+    const checkForWinner = (theGameBoard) => {
+        let check = [];
+        let checkH = checkHorizontal(theGameBoard);
+        check.push(checkH);
+        let checkV = checkVertical(theGameBoard);
+        check.push(checkV);
+        let checkD = checkDiagonal(theGameBoard);
+        check.push(checkD);
+        for(let i = 0; i < check.length; i++) {
+            if(check[i] != false) {
+                return check[i];
+            }
+        };  
+        return false;
+    }
+
+    function checkHorizontal(theGameBoard) {
+        for(let i = 0; i < theGameBoard.length; i += 3) {
+            let testArray = [];
+            for(let j = i; j < i + 3; j++) {
+                testArray.push(theGameBoard[j].textContent);
+            }
+            let concatTestArray = testArray.join('');
+            console.log(concatTestArray);
+            console.log(xCompareArray);
+            console.log(yCompareArray);
+            if(concatTestArray === xCompareArray) {
+                return 'X has won';
+            }
+            else if(concatTestArray === yCompareArray) {
+                return 'O has won';
+            }
+            else {
+                testArray.length = 0;
+                continue;
+            }
+        }
+        return false;
+    }
+
+    function checkVertical(theGameBoard) {
+        for(let i = 0; i < 3; i ++) {
+            let testArray = [];
+            for(let j = i; j < i + 7; j += 3) {
+                testArray.push(theGameBoard[j].textContent);
+            }
+            let concatTestArray = testArray.join('');
+            if(concatTestArray === xCompareArray) {
+                return 'X has won';
+            }
+            else if(concatTestArray === yCompareArray) {
+                return 'O has won';
+            }
+            else {
+                testArray.length = 0;
+                continue;
+            }
+        }
+        return false;
+    }
+
+    function checkDiagonal(theGameBoard) {
+        for(let i = 0; i < 3; i += 2) {
+            let testArray = [];
+            if(i === 0) {
+                for(let j = i; j < theGameBoard.length; j += 4) {
+                    testArray.push(theGameBoard[j].textContent);
+                }
+            }
+            else {
+                for(let j = i; j <= i + 4 ; j += 2) {
+                    testArray.push(theGameBoard[j].textContent);
+                }
+            }
+            let concatTestArray = testArray.join('');
+            if(concatTestArray === xCompareArray) {
+                return 'X has won';
+            }
+            else if(concatTestArray === yCompareArray) {
+                return 'O has won';
+            }
+            else {
+                testArray.length = 0;
+                continue;
+            }
+        }
+        return false;
+    }
+
+
+    return {
+        addClickToMark
+    };
+})();
+
 gameBoard.addGrid();
+displayController.addClickToMark();
+
